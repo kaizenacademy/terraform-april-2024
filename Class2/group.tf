934986c929f44a8b495a2596b5dff4d4b5832f03
kaizenacademy <kaizen.techacademy@gmail.com>
2024-07-04 01:31:23 +0000

    added class2 files

---
+++ Class2/group.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group" "developers2" {
+  name = "hello"
+}
+
+resource "aws_iam_group" "developers3" {
+  name = "developers3"
+}
---
+++ Class2/main.tf
@@ -0,0 +1,25 @@
+provider aws {
+    region = "us-east-2"
+}
+
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("~/.ssh/id_rsa.pub")
+# }
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = "kaizen-adilet777"
+#   force_destroy = true
+# }
+
+# resource "aws_s3_object" "object" {
+#   depends_on = [aws_s3_bucket.example]
+#   bucket = "kaizen-adilet777"
+#   key    = "main.tf"
+#   source = "main.tf"
+# }
+
+# resource "aws_s3_bucket" "example" {
+#   bucket_prefix = "kaizen-"
+#   force_destroy = true
+# }
