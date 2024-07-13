092891107c5c508cfba09cd18cd3039028017a22
kaizenacademy <kaizen.techacademy@gmail.com>
2024-07-13 21:53:56 +0000

    class 6 files

--- Class2/main.tf
+++ Class2/main.tf
@@ -2,10 +2,10 @@ provider aws {
     region = "us-east-2"
 }
 
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "deployer-key"
-#   public_key = file("~/.ssh/id_rsa.pub")
-# }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
 # resource "aws_s3_bucket" "example" {
 #   bucket = "kaizen-adilet777"
